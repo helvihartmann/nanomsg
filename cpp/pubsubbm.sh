6180adda91bc68149ba2ec211d7073e30f1cfc27
Helvi Hartmann <hhartmann@fias.uni-frankfurt.de>
2015-08-17 15:09:32 +0200

    modified run script to allow for running variable number of clients

--- cpp/pubsubbm.sh
+++ cpp/pubsubbm.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+end=4;
+
 if [[ "$SLURMD_NODENAME" ]]; then
 
     readarray -t NODELIST < <(scontrol show hostname $SLURM_NODELIST)
     if [ "$SLURM_PROCID" == 0 ]; then
         echo ${NODELIST[*]}
         ./build/pubsubbm -u tcp://*:5555 -b 8589934592 -t server
-
-    elif [ "$SLURM_PROCID" == 1 ]; then
-        ./build/pubsubbm -u tcp://${NODELIST[0]}:5555 -n client0 -b 8589934592
-    elif [ "$SLURM_PROCID" == 2 ]; then
-        ./build/pubsubbm -u tcp://${NODELIST[0]}:5555 -n client1 -b 8589934592
-    elif [ "$SLURM_PROCID" == 3 ]; then
-        ./build/pubsubbm -u tcp://${NODELIST[0]}:5555 -n client2 -b 8589934592
     fi
+
+    for ((n=1;n<$end;n++)); do
+        if [ "$SLURM_PROCID" == $n ]; then
+            name="client"$n
+
+            echo "process " $n $name
+            ./build/pubsubbm -u tcp://${NODELIST[0]}:5555 -n $name -b 8589934592
+        fi
+    done
 
 else
     #started directly--------------------------------------------------------------------------------------------------------------
